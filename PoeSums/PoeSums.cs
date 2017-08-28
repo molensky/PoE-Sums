7b47437669dc05b5559bf5607d24fa8315e13430
molensky <molensky@gmail.com>
2017-08-28 11:35:11 -0400

    Clean up unused references and commented out code

--- PoeSums/PoeSums.cs
+++ PoeSums/PoeSums.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 using PoeSums.Combinatorics;
 
 
@@ -45,9 +43,7 @@ namespace PoeSums
                 BestMatch.Add(strippedInputList);
                 return;
             }
-
-            // var inputCounts = GetItemCount(strippedInputList);
-
+            
             var minSum = FindMinFortyCombo(strippedInputList);
             while (minSum.Any())
             {
@@ -58,50 +54,6 @@ namespace PoeSums
                 }
                 minSum = FindMinFortyCombo(strippedInputList);
             }
-
-            /*
-            var possibleSums = CreateComboLists(strippedInputList, BestTotal);
-            while (possibleSums.Any())
-            {
-                var minSum = (from s in possibleSums select s.Count).Min();
-                var minSumList = (from s in possibleSums where s.Count == minSum select s).First();
-                var minSumItemCount = GetItemCount(minSumList);
-                
-                BestMatch.Add(minSumList);
-                possibleSums.Remove(minSumList);
-                foreach (var item in minSumList.Distinct())
-                {
-                    inputCounts[item] -= minSumItemCount[item];
-                }
-
-                var deleteList = new List<int>();
-                for (var i = 0; i < possibleSums.Count; i++)
-                {
-                    var tempInputCounts = inputCounts.ToDictionary(entry => entry.Key, entry => entry.Value);
-                    var repeatedSum = 0;
-                    foreach (var j in possibleSums[i])
-                    {
-                        if (tempInputCounts[j] > 0)
-                        {
-                            tempInputCounts[j] -= 1;
-                        } 
-                        else 
-                        {
-                            repeatedSum += j;
-                        }
-                    }
-                    if (repeatedSum > 0)
-                        deleteList.Add(i);
-                }
-                
-                foreach (var i in deleteList.OrderByDescending(j => j))
-                {
-                    possibleSums.RemoveAt(i);
-                }
-            }
-            */
-            
-            
         }
 
         public List<int> FindMinFortyCombo(List<int> inputList)
@@ -118,43 +70,5 @@ namespace PoeSums
             }
             return retVal;
         }
-
-        /*
-        public Dictionary<int, int> GetItemCount(List<int> l)
-        {
-            return l.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
-        }
-        */
-
-        /*
-        public List<List<int>> CreateComboLists(List<int> inputList, int desiredSum)
-        {
-            var retVal = new List<List<int>>();
-            var temp = new List<int>();
-            var count = Math.Pow(2, inputList.Count);
-            for (var i = 1; i <= count - 1; i++)
-            {
-                var str = Convert.ToString(i, 2).PadLeft(inputList.Count, '0');
-                if (str.Count(f => f == '1') < 3) continue;
-                for (var j = 0; j < str.Length; j++)
-                {
-                    if (str[j] == '1')
-                    {
-                        temp.Add(inputList[j]);
-                    }
-                }
-                if (temp.Sum() == BestTotal)
-                {
-                    retVal.Add(temp);
-                    temp = new List<int>();
-                }
-                else
-                {
-                    temp.Clear();
-                }
-            }
-            return retVal;
-        }
-        */
     }
 }
